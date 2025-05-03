9682140bdf2579f16cdedd1189dae094b185213a
jungissei <isseeeei@gmail.com>
2025-05-03 14:16:14 +0900

    モーダルをクリックで開き、Escキーで閉じるテストを追加

---
+++ tests/close/esc.spec.js
@@ -0,0 +1,16 @@
+import { test } from '@playwright/test';
+import { assertModalOpen, assertModalClose } from '../helpers';
+
+test("クリックでモーダルを開く", async ({ page }) => {
+  await page.goto('http://localhost:3001/basic/index.html');
+
+  await page.getByRole('button', { name: 'モーダルデモを開く' }).click();
+
+  // 共通の検証ロジックを呼び出し
+  await assertModalOpen(page);
+
+  await page.keyboard.press('Escape');
+
+  // 共通の検証ロジックを呼び出し
+  await assertModalClose(page);
+});
